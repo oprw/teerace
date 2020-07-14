2431dd88f5236bf2a4c4e2f827bffb7386de837f
oprw <60671815+oprw@users.noreply.github.com>
2020-07-15 00:52:04 +0300

    teerace 0.2.3

--- teerace/src/game/version.h
+++ teerace/src/game/version.h
@@ -10,7 +10,7 @@
 #define RACE_VERSION "4.0-dev"
 #define DDRACE_VERSION "1.1"
 #define MKRACE_VERSION "1.2.1"
-#define TEERACE_VERSION "0.2.2"
+#define TEERACE_VERSION "0.2.3"
 #define CLIENT_VERSION 0x0703
 static const char GAME_RELEASE_VERSION[8] = "0.7.3.1";
 #endif
